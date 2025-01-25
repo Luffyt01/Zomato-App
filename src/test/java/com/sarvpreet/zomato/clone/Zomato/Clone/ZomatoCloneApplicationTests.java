21f63a9a439d1ac4827f35054715e451a8496757
Sarvpreet Singh <luffytaaro112@gmail.com>
2025-01-25 18:43:05 +0530

    Adding initial files

---
+++ src/test/java/com/sarvpreet/zomato/clone/Zomato/Clone/ZomatoCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarvpreet.zomato.clone.Zomato.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
